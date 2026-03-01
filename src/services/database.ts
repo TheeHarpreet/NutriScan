dfb0ae2043e5494d1af25bb538ac8d1d42e9c145
Harpreet. <roopraharpreet1@gmail.com>
2026-03-01 23:23:24 +0000

    created a db file and added the tables

---
+++ src/services/database.ts
@@ -0,0 +1,26 @@
+import * as SQLite from "expo-sqlite";
+
+/**
+ * createthe local SQLite database file.
+ * the file is stored on the device.
+ */
+export const db = SQLite.openDatabaseSync("nutriscan.db");
+
+/**
+ * initialise the database tables (run once on app start).
+ * if the table already exists, it won’t be recreated.
+ */
+export function initDb() {
+  db.execSync(`
+    CREATE TABLE IF NOT EXISTS history (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      ean TEXT NOT NULL,
+      product_name TEXT,
+      brand TEXT,
+      score INTEGER,
+      source TEXT,
+      image_url TEXT,
+      scanned_at TEXT NOT NULL
+    );
+  `);
+}
